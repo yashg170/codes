8a21b8deb02fcec8a2739ce000a4051f06eb70a2
codeash123 <97959428+codeash123@users.noreply.github.com>
2022-02-07 15:43:28 +0530

    Time: 229 ms (5.08%), Space: 32.4 MB (36.98%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,17 +1,23 @@
 class Solution {
+  
 public:
-    int maxArea(int h, int w, vector<int>& hor, vector<int>& ver) {
-     int n=hor.size(); int mod=1e9+7; 
-        int m=ver.size(); 
-        sort(hor.begin(),hor.end()); sort(ver.begin(),ver.end());
-        int height=max(hor[0],h-hor[n-1]); int width=max(ver[0],w-ver[m-1]); 
-       for(int i=1; i<n;i++){
-           height=max(height,hor[i]-hor[i-1]); 
-       }
-        for(int j=1; j<m; j++){
-            width=max(width,ver[j]-ver[j-1]); 
+    long int mod=1000000007;
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+         int n=horizontalCuts.size();   int s=verticalCuts.size();
+       sort(horizontalCuts.begin(),horizontalCuts.end());   sort(verticalCuts.begin(),verticalCuts.end());
+        for(int values:verticalCuts)
+        { cout<<values<<" ";}       
+        int max1=horizontalCuts[0];     int  max2=verticalCuts[0];
+     for(int i=0; i<n-1; i++){
+           max1=max(horizontalCuts[i+1]-horizontalCuts[i],max1);
+         }
+            max1=max(max1,h-horizontalCuts[n-1]);
+         for(int i=0; i<s-1; i++){
+           max2=max(verticalCuts[i+1]-verticalCuts[i],max2);
+            
         }
-        long ans=1L*height*width; 
-        return ans%mod; 
+         max2=max(max2,w-verticalCuts[s-1]);
+        return (int)(long long)(max1%mod*max2%mod)%mod;
+        
     }
 };
