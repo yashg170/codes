a3210ec44a42cece43541744aee5483e7fbc3bcb
Yash Gupta <97959428+codeash123@users.noreply.github.com>
2022-02-14 14:43:02 +0530

    Time: 228 ms (64.49%), Space: 11.4 MB (22.17%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        bool dp[n+1]; for(bool &value:dp) value=true; 
+            int count=0;
+        dp[0]=false;
+        if(n>0){
+        dp[1]=false;
+        for(int i=2; i<n; i++){
+            if(dp[i]==true){
+                for(int j=2*i; j<n; j=j+i)
+                    dp[j]=false;
+                
+            }
+        }
+        }     
+        for(int i=0; i<n; i++){
+            if(dp[i]==true) count++;
+        }
+      return count;  
+    }
+};
