2d46f591ac359897d4ef2ae0dafd8bc5bb82f156
Yash Gupta <97959428+codeash123@users.noreply.github.com>
2022-02-14 16:54:09 +0530

    Time: 148 ms (92.38%), Space: 11.5 MB (20.63%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -6,9 +6,9 @@ public:
         dp[0]=false;
         if(n>0){
         dp[1]=false;
-        for(int i=2; i<n; i++){
+        for(int i=2; i*i<n; i++){
             if(dp[i]==true){
-                for(int j=2*i; j<n; j=j+i)
+                for(int j=i*i; j<n; j=j+i)
                     dp[j]=false;
                 
             }
