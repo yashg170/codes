e3d8a695e1dcbf6f6e8635be28eac06854e0c230
Yash Gupta <97959428+yashg170@users.noreply.github.com>
2022-02-20 12:43:12 +0530

    Time: 669 ms (16.27%), Space: 10 MB (99.61%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size(); 
+         vector<int>temp;
+    for(int i=0; i<n; i++){
+         for(int j=i+1; j<n; j++){
+             if(nums[j]+nums[i]==target){ temp.push_back(i); temp.push_back(j);}
+         }
+    }
+        return temp;
+    }
+};
