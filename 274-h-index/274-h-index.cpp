f2515062549cde65f2044cb5d4838d6b10477daf
Yash Gupta <97959428+yashg170@users.noreply.github.com>
2022-02-22 10:26:29 +0530

    Time: 3 ms (71.47%), Space: 8.7 MB (41.21%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size(); int count=1; int a=0;
+         sort(citations.begin(),citations.end());
+        int i=n-1;
+        while( i>=0 ){
+            if(count<=citations[i] ){
+              a=count;
+            count++;
+            }
+            i--;
+     }
+        return a;
+    }
+};
